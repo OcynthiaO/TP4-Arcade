9adb9227f8d36026fbe11fc32db993db2a54a4c9
zhangc <zhangc@educ.college-montreal.qc.ca>
2024-01-18 10:39:56 -0500

    TP4

--- main.py
+++ main.py
@@ -19,12 +19,13 @@ class Balle:
 
         if self.x < self.rayon:
             self.x = self.rayon
+            self.change_x*=-2
 
         if self.x > SCREEN_WIDTH - self.rayon:
             self.x = SCREEN_WIDTH - self.rayon
 
         if self.y < self.rayon:
-            self.y = self.rayon
+            self.change_y*=-2
 
         if self.y > SCREEN_HEIGHT - self.rayon:
             self.y = SCREEN_HEIGHT - self.rayon
@@ -43,24 +44,38 @@ class Rectangle:
         self.color = color
         self.angle = angle
 
-    def update(self):
-        self.x += self.change_x
-        self.y += self.change_y
+    class Balle:
+        def __init__(self, x, y, change_x, change_y, rayon, color):
+            self.x = x
+            self.y = y
+            self.change_x = change_x
+            self.change_y = change_y
+            self.rayon = rayon
+            self.color = color
 
-        if self.x < self.width / 2:
-            self.x = self.width / 2
+        def update(self):
+            self.x += self.change_x
+            self.y += self.change_y
 
-        if self.x > SCREEN_WIDTH - self.width / 2:
-            self.x = SCREEN_WIDTH - self.width / 2
+            if self.x < self.rayon:
+                self.x = self.rayon
+                self.change_x *= -1
 
-        if self.y < self.height / 2:
-            self.y = self.height / 2
+            if self.x > SCREEN_WIDTH - self.rayon:
+                self.x = SCREEN_WIDTH - self.rayon
+                self.change_x *= -1
 
-        if self.y > SCREEN_HEIGHT - self.height / 2:
-            self.y = SCREEN_HEIGHT - self.height / 2
+            if self.y < self.rayon:
+                self.y = self.rayon
+                self.change_y *= -1
 
-    def draw(self):
-        arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.color, self.angle)
+            if self.y > SCREEN_HEIGHT - self.rayon:
+                self.y = SCREEN_HEIGHT - self.rayon
+                self.change_y *= -1
+
+        def draw(self):
+            arcade.draw_circle_filled(self.x, self.y, self.rayon, self.color)
+
 
 class MyGame(arcade.Window):
     def __init__(self):
@@ -81,11 +96,11 @@ class MyGame(arcade.Window):
 
     def on_mouse_press(self, x, y, button, modifiers):
         if button == arcade.MOUSE_BUTTON_LEFT:
-            balle = Balle(x, y, 0, 0, random.randint(10, 30), arcade.color.RED)
+            balle = Balle(x, y, 2, 2, random.randint(10, 30), arcade.color.RED)
             self.balles.append(balle)
 
         elif button == arcade.MOUSE_BUTTON_RIGHT:
-            rectangle = Rectangle(x, y, 0, 0, random.randint(20, 50), random.randint(20, 50), arcade.color.BLUE, 0)
+            rectangle = Rectangle(x, y, 2, 2, random.randint(20, 50), random.randint(20, 50), arcade.color.BLUE, 0)
             self.rectangles.append(rectangle)
 
     def on_update(self, delta_time):
